b39e1f7cab7d741077443a125ebeaf974f5464a2
Alexandra Moik <moik.alexandra@gmail.com>
2023-05-16 13:36:15 +0200

    move function is introduced
    to be able to make a move on the board

--- project_one.py
+++ project_one.py
@@ -8,3 +8,11 @@ def evaluate (board):
         return '!'
     else:
         return'-'
+    
+def move (board, position_number, mark):
+    # returns the game board with given mark on given position
+    position_number_trans = position_number -1 
+    # because python starts to count at 0 and then it would always be the chosen position + 1
+    
+    board = board[:position_number_trans] + mark + board[position_number_trans + 1:20]
+    return board
